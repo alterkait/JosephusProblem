c23e62e8e787332333b76409e32a831cfae7d83b
alterkait <alterkait2001@icloud.com>
2017-10-24 12:46:18 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,21 @@ Programs runs solution, outputtin the winning place in the circle.
 using namespace std;
 int main()
 {
+  int nOfSoldiers;
+  cout << "How many soldiers are in the Initial circle?" << endl;
+  cin >> nOfSoldiers;
   
+  // if n = 2 ** a, winner = 1//
+  
+  int a, winnerSoldier;
+  a = 1000;
+  if (nOfSoldiers == 2 ** a)
+  {
+    winnerSoldier = 1;
+  }
+  
+  
+  cout << "The last Soldier standing is" << winnerSoldier << endl; 
+  
+  return 0;
 }
