a256cf9b18af6578de53ea216f6b3bd540c4bac3
alterkait <alterkait2001@icloud.com>
2017-10-24 09:03:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 /* Program Structure:
 User inputs the number of people starting in the initial circle.
+Program outputs actions performed, e.g. '2' kills '3', etc..
 Programs runs solutio, outputtin the winning place in the circle.
 */
 
