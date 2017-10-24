9cdf58333f19da4b926a8c47de3893469964e8a2
alterkait <alterkait2001@icloud.com>
2017-10-24 09:00:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 
+/* Program Structure:
+User inputs the number of people starting in the initial circle.
+Programs runs solutio, outputtin the winning place in the circle.
+*/
+
 using namespace std;
 int main()
 {
